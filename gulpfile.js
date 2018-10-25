126da9c503141f1d8058ef2e80b6460717146114
karahenderson <karaelyse@utexas.edu>
2018-10-25 17:03:02 -0500

    bootstrap updates with pirates

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var gutil       = require('gulp-util');
 
 // Compile sass into CSS and put into docs/css folder
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['src/scss/*.scss'])
         .pipe(sass())
+        .on('error', gutil.log) //new linegit 
         .pipe(gulp.dest("docs/css"))
         .pipe(browserSync.stream());
 });
